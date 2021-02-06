7340b56636e16cc8ab820deedb63be56d977b6bd
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-02-06 18:52:24 +0000

    Css for Maths Addition
    Created CSS file for Maths Addition HTML page in Javascript and React.

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+//Add two random numbers and click Calulate to check answer.
+let num1 = Math.floor(Math.random()*10);
+let num2 = Math.floor(Math.random()*10);
+let sum = 0;
+function addNumbers(num1,num2){
+   return sum = num1+num2;
+}
+
+console.log(sum);
+
--- react/calculation/src/index.js
+++ react/calculation/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import Addition from './addition.jsx';
+import CSS from './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
