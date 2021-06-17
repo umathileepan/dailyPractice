1cde31c9c1ac43a0cf7f113605372bc889d6c100
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-06-17 06:57:59 +0100

    create a counter app using React

---
+++ MoshReact/counter-app/src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    
+    render() { 
+        return (
+            <div> 
+                <h1>Hello World!</h1> 
+                <button>Increament</button>
+            </div> 
+            );
+    }
+}
+ 
+export default Counter;
--- react/calculation/src/hello.jsx
+++ react/calculation/src/hello.jsx
@@ -3,6 +3,7 @@ import React  from 'react';
 
 class Hello extends React.Component{
 
+    
     render() {
         return(
             <div>
