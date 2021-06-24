6a41283cf484d8b255d7c209daf36fe513e0516b
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-06-24 19:53:13 +0100

    created multiple components using React

--- MoshReact/counter-app/src/components/counter.jsx
+++ MoshReact/counter-app/src/components/counter.jsx
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count: 0,
-        tags: ['tag1','tag2','tag3']
+        value: this.props.value
     };
 
     handleIncrement = product => {
-        console.log(product);
-        this.setState({count: this.state.count + 1});
+        this.setState({value: this.state.value + 1});
     };
 
     render() { 
@@ -18,30 +16,19 @@ class Counter extends Component {
             <div> 
                 <span className= {this.getBadgeClasses()}>{this.formatCount()}</span> 
                 <button onClick={() => this.handleIncrement(this.product)} className= "btn btn-secondary btn-sm">Increament</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}
-                </ul>
-                {this.state.tags.length===0 && "Please create new tags"}
-                {this.renderTags()}
             </div> 
             );
     }
 
-    renderTags() {
-        if(this.state.tags.length===0) return <p>There are no tags!</p>;
-
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li> )}</ul>;
-    }
-
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count;
+        const {value} = this.state;
+        return value === 0 ? "Zero" : value;
     }
 }
  
--- MoshReact/counter-app/src/index.js
+++ MoshReact/counter-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/counter';
+import Counters from './components/counters';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>,
   document.getElementById('root')
 );
